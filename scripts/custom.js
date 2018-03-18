19f6e0a1f2dda4a5fb19e6ca3746c78524feca08
Stephanie Dorr <stephaniedorr@Stephanies-iMac.local>
2018-03-17 21:16:25 -0400

    dds review updates

--- scripts/custom.js
+++ scripts/custom.js
@@ -9,8 +9,8 @@ var scrollLink = $('.slide-section');
 scrollLink.click(function(e) {
 	e.preventDefault();
 	$('body,html').animate({
-		scrollTop: $(this.hash).offset().top - 130
-	}, 2000)
+		scrollTop: $(this.hash).offset().top - 100
+	}, 1000)
 });
 
 var toTop = $('.slide-top');
@@ -19,10 +19,41 @@ toTop.click(function(e){
 	e.preventDefault();
 	$('body,html').animate ({
 		scrollTop : 0
-	}, 2000);
+	}, 1000);
 });
 
 });
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
 
 
 
